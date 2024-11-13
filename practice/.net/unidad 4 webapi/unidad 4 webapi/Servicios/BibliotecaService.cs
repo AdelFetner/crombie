5df73a5ef271f70c7a7653edca3f430c7efb732e
adelfetner <adelfetner@gmail.com>
2024-11-12 21:27:50 -0300

    Added new project, webapi test

---
+++ practice/.net/unidad 4 webapi/unidad 4 webapi/Servicios/BibliotecaService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using unidad_4_webapi;
+using UsuarioComponent;
+
+namespace unidad_4_webapi.Servicios
+
+{
+    public class BibliotecaService
+    {
+        public ArrayList Libros
+        {
+            get
+            {
+                return Libros;
+            }
+            set
+            {
+                if (Libros.Count == 0)
+                {
+                    Console.WriteLine("Actualmente no hay libros en la biblioteca");
+                };
+            }
+        }
+        public ArrayList LibrosDisponibles
+        {
+            get
+            {
+                return LibrosDisponibles;
+            }
+            set
+            {
+                if (LibrosDisponibles.Count == 0)
+                {
+                    Console.WriteLine("Actualmente no hay libros disponibles");
+                };
+            }
+        }
+
+        public ArrayList Usuarios
+        {
+            get
+            {
+                return Usuarios;
+            }
+            set
+            {
+                Usuarios = value;
+            }
+        }
+
+        public string NuevoLibro(LibroService libro)
+        {
+            // titulo
+            Console.WriteLine("Ingrese el título del libro:");
+            string titulo = Console.ReadLine();
+            if (titulo.Length == 0 || titulo == null)
+            {
+                throw new Exception("Debe haber un titulo");
+            }
+            libro.Titulo = titulo;
+            //  autor
+            Console.WriteLine("Ingrese el autor del libro:");
+            string autor = Console.ReadLine();
+            if (autor.Length == 0 || autor == null)
+            {
+                throw new Exception("Debe haber un autor");
+            }
+            libro.Autor = autor;
+            //isbn
+            Console.WriteLine("Ingrese el isbn del libro:");
+            string isbn = Console.ReadLine();
+            if (isbn.Length == 0 || isbn == null)
+            {
+                throw new Exception("Debe haber un ISBN");
+            }
+            libro.ISBN = isbn;
+
+            Libros.Add(libro);
+            return "Libro agregado exitosamente";
+        }
+
+        public string NuevoUsuario(Usuario usuario)
+        {
+            Console.WriteLine("Ingrese el nombre del usuario:");
+            string nombre = Console.ReadLine();
+            if (nombre.Length == 0 || nombre == null)
+            {
+                throw new Exception("Debe haber un nombre");
+            }
+            usuario.Nombre = nombre;
+
+            Console.WriteLine("Ingrese el ID del usuario:");
+            int userID = Convert.ToInt32(Console.ReadLine());
+            if (userID.GetType() == typeof(int))
+            {
+                throw new Exception("Debe haber un ID");
+            }
+            usuario.UserID = userID;
+
+            Usuarios.Add(usuario);
+            return "Usuario creado exitosamente";
+        }
+
+        public string PrestarLibro(LibroService libro, Usuario usuario)
+        {
+            Console.WriteLine("Ingrese el ISBN del libro a prestar:");
+            string isbn = Console.ReadLine();
+            libro.ISBN = isbn;
+
+            Console.WriteLine("Ingrese el ID del usuario a prestar:");
+            string userID = Console.ReadLine();
+            usuario.UserID = userID;
+
+            //cambia obj usuariopresta y como no disponible
+            libro.EstaDisponible = false;
+            libro.UsuarioIDPresta = usuario.UserID;
+
+            usuario.LibrosPrestados.Add(libro);
+
+            return "Libro prestado exitosamente";
+        }
+
+        public string DevolverLibro(string ISBN, int IUserID)
+        {
+            Console.WriteLine("Ingrese el ISBN del libro a devolver:");
+            string isbn = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el ID del usuario que devuelve:");
+            string userID = Console.ReadLine();
+            if (Usuario.)
+
+                libro.EstaDisponible = true;
+            //libro.UsuarioIDPresta = 
+        }
+    }
+}
---
+++ practice/.net/unidad 4 webapi/unidad 4 webapi/Servicios/LibroService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unidad_4_webapi.Servicios
+{
+    public class LibroService
+    {
+        private string _autor;
+        public string Autor
+        {
+            get
+            {
+                return _autor;
+            }
+            set
+            {
+                if (value.Length < 5)
+                {
+                    Console.WriteLine($"Nombre Invalido");
+                };
+                _autor = value;
+            }
+        }
+
+        public string Titulo { get; set; }
+        public string ISBN { get; set; }
+        public bool EstaDisponible { get; set; }
+        public int UsuarioIDPresta { get; set; }
+
+        public LibroService(string Autor, string Titulo, string ISBN, bool EstaDisponible)
+        {
+            _autor = Autor;
+            this.Autor = Autor;
+            this.Titulo = Titulo;
+            this.ISBN = ISBN;
+            this.EstaDisponible = EstaDisponible;
+        }
+
+        public LibroService()
+        {
+            _autor = Autor;
+            Autor = "Autor por defecto";
+            Titulo = "Titulo por defecto";
+            ISBN = "ISBN por defecto";
+            EstaDisponible = false;
+        }
+    }
+}
