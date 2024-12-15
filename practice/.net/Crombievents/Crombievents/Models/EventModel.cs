52e191a74caf87b449655c88e1a2e8b8f769a8eb
Adel Fetner <adelfetner@gmail.com>
2024-12-15 08:09:56 -0300

    added eventmodel

--- practice/.net/Crombievents/Crombievents/Models/EventModel.cs
+++ practice/.net/Crombievents/Crombievents/Models/EventModel.cs
@@ -2,5 +2,12 @@
 {
     public class EventModel
     {
+        public int EventID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public string Place { get; set; }
+        public int MaxCapacity { get; set; }
+        public int OrganizerID { get; set; }
     }
 }
