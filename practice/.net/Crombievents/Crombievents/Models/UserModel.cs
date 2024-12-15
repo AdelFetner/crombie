f6e77f7bc79f21eae21967a635e14873ac0cfa25
Adel Fetner <adelfetner@gmail.com>
2024-12-15 06:05:09 -0300

    Added usermodel

--- practice/.net/Crombievents/Crombievents/Models/UserModel.cs
+++ practice/.net/Crombievents/Crombievents/Models/UserModel.cs
@@ -2,5 +2,10 @@
 {
     public class UserModel
     {
+            public int UsuarioID { get; set; }
+            public string Nombre { get; set; }
+            public string Email { get; set; }
+            public string Telefono { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
     }
 }
