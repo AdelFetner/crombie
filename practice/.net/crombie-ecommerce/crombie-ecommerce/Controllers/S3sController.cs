99dd6b107e7624a9a3380b134d0a74820143a5d4
Adel Fetner <adelfetner@gmail.com>
2025-01-23 13:44:51 -0300

    added upload validations to s3controller

--- practice/.net/crombie-ecommerce/crombie-ecommerce/Controllers/S3sController.cs
+++ practice/.net/crombie-ecommerce/crombie-ecommerce/Controllers/S3sController.cs
@@ -49,11 +49,29 @@ namespace crombie_ecommerce.Controllers
         {
             try
             {
+                // Checks for null and empty files
                 if (fileObject == null || fileObject.Length == 0)
                     return BadRequest("File is missing");
 
+                // Checks for any invalid characters in the fileName
+                if (fileObject.FileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)
+                    return BadRequest("File name contains invalid characters");
+
                 using var stream = fileObject.OpenReadStream();
-                var fileName = $"{Guid.NewGuid()}-{fileObject.FileName}";
+
+                // checks if it is a readable stream (e.g: think of corrupted or locked files)
+                if (!stream.CanRead)
+                    return BadRequest("File stream is not readable");
+
+                // Checks for a file size of 1MB. Length on files is based on bytes, and each multiplier is separated by 1024 units. Byte -> KiloByte -> MegaByte
+                if (stream.Length > 1 * 1024 * 1024)
+                    return BadRequest("File size is too large, maximum up to 1MB");
+
+                // Checks for a content type that equals jpeg/jpg or png
+                if (!fileObject.ContentType.Equals("image/jpeg") && !fileObject.ContentType.Equals("image/png"))
+                    return BadRequest($"File type is not supported, only jpg and png are allowed. Your file type: {fileObject.ContentType}");
+
+                var fileName = $"products/{Guid.NewGuid()}-{fileObject.FileName}";
 
                 var url = await _s3Service.UploadFileAsync(stream, fileName, fileObject.ContentType);
 
