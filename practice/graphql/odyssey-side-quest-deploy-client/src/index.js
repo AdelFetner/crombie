6cad9f551ba5de0f13d6a169b03b5b79dfee8ec5
Adel Fetner <adelfetner@gmail.com>
2025-03-05 06:50:19 -0300

    feat: Done side quest, Deploying an Apollo app with Railway

---
+++ practice/graphql/odyssey-side-quest-deploy-client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://catstronauts-client-production-29bf.up.railway.app/",
+    cache: new InMemoryCache(),
+    name: "web",
+    version: "1.0",
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <GlobalStyles />
+        <Pages />
+    </ApolloProvider>,
+    document.getElementById("root")
+);
